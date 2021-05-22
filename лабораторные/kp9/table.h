e94829c6562b60c2ecd1bfaca6885e4b51f50a78
AnnVarlamova <74834132+AnnVarlamova@users.noreply.github.com>
2021-05-22 09:18:33 +0300

    Add files via upload

---
+++ лабораторные/kp9/table.h
@@ -0,0 +1,20 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef struct line {
+    char data[100];
+} line;
+
+bool exists(double keys[], double item, int n);
+int binary_search(double keys[], double item, int low, int high);
+bool is_sorted(double keys[], int n);
+bool is_reversed(double keys[], int n);
+void swap_key(double *a, double *b);
+void swap_line(line *a, line *b);
+void reverse_array(double keys[], int n, line table[]);
+void selection_sort_with_exchanges(double keys[], int n, line table[]);
+
+#endif
